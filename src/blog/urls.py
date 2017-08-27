04cee8df3277a8e4e574bb804c381e10b2619248
DadaDu0043 <yingda_du@yahoo.com>
2017-08-27 01:06:13 -0400

    set up blog GRUD

---
+++ src/blog/urls.py
@@ -0,0 +1,41 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from posts.views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_update,
+    post_delete,
+)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^posts/', include("posts.urls", namespace='posts')),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
+    url(r'^$', post_list, name='list'),
+    url(r'^create/$', post_create),
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
